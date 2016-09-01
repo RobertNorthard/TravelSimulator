65137d4dfdcbd56f4be3f334435facae4269f9de
Kieran Flay <kieranflay64@gmail.com>
2016-09-02 00:09:54 +0100

    bug fix

--- TravelSimulatorCode/app/src/main/java/com/walking/walkingsimulator/MapsActivity.java
+++ TravelSimulatorCode/app/src/main/java/com/walking/walkingsimulator/MapsActivity.java
@@ -4,14 +4,17 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,22 +28,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
-    private String TAG = getLocalClassName().toString();
-    private GoogleMap googleMap;
+    private GoogleMap mMap;
 
-    LatLng myLocation;
-    LatLng myDestination;
+    LatLng myLoc;
+    LatLng myDes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map inputStream ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -48,49 +57,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
+        mMap = googleMap;
 
         LocationManager locationManager = (LocationManager)
                 getSystemService(Context.LOCATION_SERVICE);
+
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
 
+
+
+
         if (location == null) {
-            Log.e(TAG, "Unable to get location.");
-        } else {
+
+        }
+        else {
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
 
-            this.googleMap.setMinZoomPreference(15);
-            this.googleMap.setOnMapClickListener(this);
+            mMap.setMinZoomPreference(15);
+            mMap.setOnMapClickListener(this);
 
-            myLocation = new LatLng(latitude, longitude);
-            this.googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
-            this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+            // Add a marker in Sydney and move the camera
+            myLoc = new LatLng(latitude, longitude);
+            mMap.addMarker(new MarkerOptions().position(myLoc).title("My Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLoc));
         }
     }
 
+
     @Override
     public void onMapClick(LatLng latLng) {
-        if (myDestination != null) {
-            googleMap.clear();
-            googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        if (myDes != null) {
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(myLoc).title("My Location"));
         }
-        myDestination = new LatLng(latLng.latitude, latLng.longitude);
-        googleMap.addMarker(new MarkerOptions().position(myDestination).title("Destination"));
+        myDes = new LatLng(latLng.latitude, latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(myDes).title("Destination"));
 
-        String url = makeURL(myLocation.latitude, myLocation.longitude, myDestination.latitude, myDestination.longitude);
-
-        connectAsyncTask connectAsyncTask = new connectAsyncTask(url);
-        connectAsyncTask.execute();
+        String url =   makeURL(myLoc.latitude, myLoc.longitude, myDes.latitude, myDes.longitude);
+        connectAsyncTask c = new connectAsyncTask(url);
+        c.execute();
     }
 
     public String makeURL (double sourcelat, double sourcelog, double destlat, double destlog ){
         StringBuilder urlString = new StringBuilder();
-        urlString.append("https://maps.googleapis.com/maps/api/directions/jsonResponse");
+        urlString.append("https://maps.googleapis.com/maps/api/directions/json");
         urlString.append("?origin=");// from
         urlString.append(Double.toString(sourcelat));
         urlString.append(",");
@@ -101,8 +116,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .append(Double.toString( destlat));
         urlString.append(",");
         urlString.append(Double.toString( destlog));
-
-        // Make sure to use your own generated key
         urlString.append("&sensor=false&mode=driving&alternatives=true");
         urlString.append("&key=AIzaSyCBuSXNUsLY46FU6gCuUKsDtmnn_bY37LE");
         return urlString.toString();
@@ -110,34 +123,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void drawPath(String  result) {
 
+        System.out.println("RESULT" + result);
+
         try {
+            //Tranform the string into a json object
             final JSONObject json = new JSONObject(result);
             JSONArray routeArray = json.getJSONArray("routes");
             JSONObject routes = routeArray.getJSONObject(0);
             JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
             String encodedString = overviewPolylines.getString("points");
-            List<LatLng> latLngList = decodePoly(encodedString);
-            Polyline line = googleMap.addPolyline(new PolylineOptions()
-                    .addAll(latLngList)
+            List<LatLng> list = decodePoly(encodedString);
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .addAll(list)
                     .width(12)
                     .color(Color.parseColor("#05b1fb"))//Google maps blue color
                     .geodesic(true)
             );
         }
         catch (JSONException e) {
-            Log.e(TAG, "Error: " + e);
+
         }
+
     }
 
     private class connectAsyncTask extends AsyncTask<Void, Void, String>{
-
         private ProgressDialog progressDialog;
         String url;
-
         connectAsyncTask(String urlPass){
             url = urlPass;
         }
-
         @Override
         protected void onPreExecute() {
             // TODO Auto-generated method stub
@@ -147,19 +161,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             progressDialog.setIndeterminate(true);
             progressDialog.show();
         }
-
         @Override
         protected String doInBackground(Void... params) {
             JSONParser jParser = new JSONParser();
             String json = jParser.getJSONFromUrl(url);
             return json;
         }
-
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             progressDialog.hide();
-            if(result != null){
+            if(result!=null){
                 drawPath(result);
             }
         }
