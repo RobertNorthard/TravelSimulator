46963068170864a4e5526ac7a8b9b67e3fca5e8b
Kieran Flay <kieranflay64@gmail.com>
2016-09-12 23:40:07 +0100

    -f Adding icons and starter code

--- TravelSimulatorCode/app/src/main/java/com/walking/walkingsimulator/MapsActivity.java
+++ TravelSimulatorCode/app/src/main/java/com/walking/walkingsimulator/MapsActivity.java
@@ -4,17 +4,20 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
-import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,22 +31,25 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener,  View.OnClickListener  {
 
-    private GoogleMap mMap;
+    private GoogleMap googleMap;
 
-    LatLng myLoc;
-    LatLng myDes;
+    LatLng myLocation;
+    LatLng myDestination;
+
+    Location location;
+    LocationManager locationManager;
+
+    private static final String TAG = MapsActivity.class.getName();
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,52 +59,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ImageButton btnRouteType = (ImageButton) findViewById(R.id.btnRouteType);
+        btnRouteType.setOnClickListener(this);
+
+        ImageButton btnSettings = (ImageButton) findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(this);
+
+        ImageButton btnUndo = (ImageButton) findViewById(R.id.btnUndo);
+        btnUndo.setOnClickListener(this);
+
+        Button btnGo = (Button) findViewById(R.id.btnGo);
+        btnGo.setOnClickListener(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        this.googleMap = googleMap;
 
-        LocationManager locationManager = (LocationManager)
-                getSystemService(Context.LOCATION_SERVICE);
-
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
 
+            // We now have permission, so load map now in onRequestPermissionsResult
+        } else {
+            location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
 
-
-
-        if (location == null) {
-
-        }
-        else {
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-
-            mMap.setMinZoomPreference(15);
-            mMap.setOnMapClickListener(this);
-
-            // Add a marker in Sydney and move the camera
-            myLoc = new LatLng(latitude, longitude);
-            mMap.addMarker(new MarkerOptions().position(myLoc).title("My Location"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLoc));
+            // Load the map because we have permission already
+            loadMap(location);
         }
     }
 
+    public void loadMap(Location location) {
+
+        if (location == null) {
+            Log.e(TAG, "Error - Unable to get location");
+        } else {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            this.googleMap.setMinZoomPreference(15);
+            this.googleMap.setOnMapClickListener(this);
+
+            // Add a marker in Sydney and move the camera
+            myLocation = new LatLng(latitude, longitude);
+            this.googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+            this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                    loadMap(location);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MapsActivity.this, "Permission denied to read your Location", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
     @Override
     public void onMapClick(LatLng latLng) {
-        if (myDes != null) {
-            mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(myLoc).title("My Location"));
+        if (myDestination != null) {
+            googleMap.clear();
+            googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
         }
-        myDes = new LatLng(latLng.latitude, latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(myDes).title("Destination"));
+        myDestination = new LatLng(latLng.latitude, latLng.longitude);
+        googleMap.addMarker(new MarkerOptions().position(myDestination).title("Destination"));
 
-        String url =   makeURL(myLoc.latitude, myLoc.longitude, myDes.latitude, myDes.longitude);
+        String url =   makeURL(myLocation.latitude, myLocation.longitude, myDestination.latitude, myDestination.longitude);
         connectAsyncTask c = new connectAsyncTask(url);
         c.execute();
     }
@@ -133,7 +185,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
             String encodedString = overviewPolylines.getString("points");
             List<LatLng> list = decodePoly(encodedString);
-            Polyline line = mMap.addPolyline(new PolylineOptions()
+            Polyline line = googleMap.addPolyline(new PolylineOptions()
                     .addAll(list)
                     .width(12)
                     .color(Color.parseColor("#05b1fb"))//Google maps blue color
@@ -141,9 +193,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             );
         }
         catch (JSONException e) {
-
+            Log.e(TAG, "Error - Unable to get draw path");
         }
+    }
 
+    @Override
+    public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.btnGo:
+                    Toast.makeText(this, "Pressed GO button", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btnRouteType:
+                    Toast.makeText(this, "Pressed Route Type button", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btnUndo:
+                    Toast.makeText(this, "Pressed Undo button", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btnSettings:
+                    Toast.makeText(this, "Pressed Settings button", Toast.LENGTH_SHORT).show();
+                    break;
+            }
     }
 
     private class connectAsyncTask extends AsyncTask<Void, Void, String>{
@@ -154,7 +223,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         @Override
         protected void onPreExecute() {
-            // TODO Auto-generated method stub
+
             super.onPreExecute();
             progressDialog = new ProgressDialog(MapsActivity.this);
             progressDialog.setMessage("Fetching route, Please wait...");
